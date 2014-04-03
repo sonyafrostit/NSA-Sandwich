7d80c650319f9a26ca15a3a2a416c76eb1a92498
hasbik <bikashmunikar@my.unt.edu>
2014-04-03 13:13:52 -0500

    emulate credit

---
+++ Emulate Credit/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace emulate_credit
+{
+    public partial class Form1 : Form
+    {
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (CCBox.TextLength == 16)
+            {
+                Form2 f2 = new Form2();
+                f2.passValue = CCBox.Text;
+                f2.ShowDialog();
+                Close();
+            }
+
+            else
+            { checkLabel.Text = "Invalid card length!"; }
+
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Emulate Credit/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace emulate_credit
+{
+    public partial class Form2 : Form
+    {
+        private string creditcardnum;
+
+        public string passValue
+        {
+            get { return creditcardnum; }
+            set { creditcardnum = value; }
+        }
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            string last_four_digit = creditcardnum.Substring(creditcardnum.Length - 4, 4);
+            label2.Text = "Your transaction has been processed. \n" + "Credit Card Number: "  + "XXXX-XXXX-XXXX-" + last_four_digit;
+
+        }
+    }
+}
