cd6d990f3f9086285fb3e534c6935fee56b02f60
Dustin Fennessy <dustinfennessy@gmail.com>
2014-04-04 19:33:30 -0500

    ComponentParser class created
    Completed ComponentParser
    Class should be able to parse a component and display it in its native
    language

---
+++ CustomerInterface/CustomerInterface/ComponentParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Resources;
+using System.Globalization;
+
+namespace CustomerInterface
+{
+    class ComponentParser
+    {
+        private CultureInfo ci;
+
+        public ComponentParser(CultureInfo c)
+        {
+            ci = c;
+        }
+
+        public string parseComponent(string component)
+        {
+            string translatedWord;
+            Assembly a = Assembly.Load("CustomerInterface");
+            ResourceManager rm = new ResourceManager("CustomerInterface.Lang.components", a);
+            translatedWord = rm.GetString(component.ToLower(), ci);
+            return translatedWord;
+        }
+    }
+}
