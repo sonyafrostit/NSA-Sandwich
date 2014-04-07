ba8219d9e948db775e91619874639727abdaaa39
unknown <LibCMNTYUser@LCDDD1122LW7KM1.unt.ad.unt.edu>
2014-04-07 16:44:42 -0500

    GUI loyalty created

---
+++ CustomerInterface/CustomerInterface/LogIn.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace CustomerInterface
+{
+    partial class LogInForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.enterInfoLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(291, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // enterInfoLabel
+            // 
+            this.enterInfoLabel.AutoSize = true;
+            this.enterInfoLabel.Location = new System.Drawing.Point(13, 13);
+            this.enterInfoLabel.Name = "enterInfoLabel";
+            this.enterInfoLabel.Size = new System.Drawing.Size(149, 13);
+            this.enterInfoLabel.TabIndex = 1;
+            this.enterInfoLabel.Text = "Enter in your account number:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Sign in";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LogInForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 108);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.enterInfoLabel);
+            this.Controls.Add(this.textBox1);
+            this.Name = "LogInForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label enterInfoLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ CustomerInterface/CustomerInterface/LogInOrGuest.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CustomerInterface
+{
+    partial class LogInOrGuest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.signInButton = new System.Windows.Forms.Button();
+            this.guestButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // signInButton
+            // 
+            this.signInButton.Location = new System.Drawing.Point(36, 28);
+            this.signInButton.Name = "signInButton";
+            this.signInButton.Size = new System.Drawing.Size(208, 125);
+            this.signInButton.TabIndex = 2;
+            this.signInButton.Text = "Sign into loyalty account";
+            this.signInButton.UseVisualStyleBackColor = true;
+            this.signInButton.Click += new System.EventHandler(this.signInButton_Click);
+            // 
+            // guestButton
+            // 
+            this.guestButton.Location = new System.Drawing.Point(276, 28);
+            this.guestButton.Name = "guestButton";
+            this.guestButton.Size = new System.Drawing.Size(208, 125);
+            this.guestButton.TabIndex = 3;
+            this.guestButton.Text = "Continue as Guest";
+            this.guestButton.UseVisualStyleBackColor = true;
+            this.guestButton.Click += new System.EventHandler(this.guestButton_Click);
+            // 
+            // LogInOrGuest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 189);
+            this.Controls.Add(this.guestButton);
+            this.Controls.Add(this.signInButton);
+            this.Name = "LogInOrGuest";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button signInButton;
+        private System.Windows.Forms.Button guestButton;
+    }
+}
