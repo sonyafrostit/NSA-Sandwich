c515683c9e4a4c8edc14d3aa38d31515cd6ebccc
unknown <LibCMNTYUser@LCDDD1122LF7KM1.unt.ad.unt.edu>
2014-04-07 16:46:56 -0500

    Merged

---
+++ CustomerInterface/CustomerInterface/LogInOrGuest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace CustomerInterface
+{
+    public partial class LogInOrGuest : Form
+    {
+        private CultureInfo ci;
+        public LogInOrGuest(CultureInfo language)
+        {
+            InitializeComponent();
+            ci = language;
+        }
+
+        private void guestButton_Click(object sender, EventArgs e)
+        {
+            KioskWindow mainForm = new KioskWindow(ci);
+            mainForm.Show();
+            Hide();
+        }
+
+        private void signInButton_Click(object sender, EventArgs e)
+        {
+            LogInForm logInForm = new LogInForm(ci);
+            logInForm.Show();
+            Hide();
+        }
+    }
+}
---
+++ CustomerInterface/CustomerInterface/createLoyalty.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace CustomerInterface
+{
+    public partial class createLoyalty : Form
+    {
+        private CultureInfo ci;
+        private string name;
+        private string email;
+
+        public createLoyalty(CultureInfo language)
+        {
+            InitializeComponent();
+            ci = language;
+        }
+
+        private void createLoyaltyBut_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                errorLabel.Text = "Enter your name!";
+            }
+
+            else if (String.IsNullOrEmpty(email))
+            {
+                errorLabel.Text = "Enter your email!";
+            }
+
+            else
+            {
+                //create account
+                KioskWindow form = new KioskWindow(ci);
+                form.Show();
+                Hide();
+            }
+        }
+
+        private void nameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            name = nameTextBox.Text;
+        }
+
+        private void emailTextBox_TextChanged(object sender, EventArgs e)
+        {
+            email = emailTextBox.Text;
+        }
+    }
+}
